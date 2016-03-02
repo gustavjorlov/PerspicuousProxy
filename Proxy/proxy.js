1a653487d687fc2a2a742a99b62c2b6e0354280e
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-03-02 22:37:02 +0100

    lots of restructuring

--- Proxy/proxy.js
+++ Proxy/proxy.js
@@ -3,19 +3,31 @@
 "use strict";
 var http = require('http'), httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({});
+var request = require('request');
 
-module.exports = {
-    run: BadProxy
-};
+http.createServer(function(req, res) {
+    handleRequest(req, res);
+}).listen(3000, function(){
+    console.log("Listening to port 3000 as proxy");
+});
 
-function BadProxy(callback){
-    http.createServer(function(req, res) {
-        callback({url: req.url});
-        handleRequest(req, res);
-    }).listen(3000, function(){
-        console.log("Listening to port 3000 as proxy");
+proxy.on('proxyRes', function (proxyRes, req, res) {
+    console.log("Sending POST to", req.url);
+    request({
+        method: "POST",
+        url: "http://localhost:8080",
+        json: true,
+        body: {
+            date: proxyRes.headers.date,
+            contentType: proxyRes.headers["content-type"],
+            url: req.url,
+            status: proxyRes.statusCode,
+            message: proxyRes.statusMessage
+        }
+    }, function(err, response, body){
+        console.log(err, body);
     });
-}
+});
 
 function handleRequest(req, res){
     proxy.web(req, res, {
---
+++ client/server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+
+var socketHandle;
+
+app.use(express.static(__dirname + "/dist"));
+app.use(bodyParser.json());
+
+app.post('/', function(req, res){
+    console.log("POST to /", req.body);
+    socketHandle.emit('log', {
+        date: req.body.date,
+        contentType: req.body.contentType,
+        url: req.body.url +"yes",
+        status: {
+            code: req.body.status,
+            message: req.body.status
+        }
+    });
+});
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('connection');
+    socket.on('event', function (data) {
+        console.log(data);
+    });
+    socketHandle = socket;
+});
+
+server.listen(8080, function(){
+    console.log("Listening to port 8080 for browser");
+});
