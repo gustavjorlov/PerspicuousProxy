bb81fc5162d7628838946fe8b1a3323d1222a9e7
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-03-03 14:09:23 +0100

    changed input style for the fail rate

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -2,30 +2,34 @@ import React from 'react';
 
 export default class Settings extends React.Component{
 
-    // constructor(){
-    //     super();
-    //     this.failvalue = 0;
-    // }
+    constructor(){
+        super();
+        this.failvalue = 0;
+    }
 
     changeScrollAttatchment(){
         // Keep scroll to bottom <input id="scollbottom" type="checkbox" />
-        // <button onClick={this.setFailRate.bind(this, 0)}>Fail none</button>
-        // <button onClick={this.setFailRate.bind(this, 0.1)}>Fail 10%</button>
-        // <button onClick={this.setFailRate.bind(this, 0.25)}>Fail 25%</button>
-        // <button onClick={this.setFailRate.bind(this, 0.75)}>Fail 75%</button>
-        // <button onClick={this.setFailRate.bind(this, 1)}>Fail all</button>
+        // <input ref="failrate" onMouseUp={this.setFailRate.bind(this)} value={this.failvalue} type="range" step="0.1" min="0" max="1" />
+        // <p ref="failvalue">0</p>
     }
-    setFailRate(){
-        console.log("this.refs.failrate", this.refs.failrate.value);
-        this.props.settings('failrate', this.refs.failrate.value);
-        this.refs.failvalue.value = this.refs.failrate.value*100 + " %";
+    setFailRate(failrate){
+        // console.log("this.refs.failrate", this.refs.failrate.value);
+        // this.failvalue = this.refs.failrate.value;
+
+        this.props.settings('failrate', failrate);
+        // this.refs.failvalue.innerHTML = this.refs.failrate.value*100 + " %";
+        // this.refs.failrate.value = this.refs.failrate.value;
     }
 
     render(){
         return (
             <div id="settings">
-                <input ref="failrate" onChange={this.setFailRate.bind(this)} type="range" step="0.1" min="0" max="1" />
-                <p ref="failvalue">0</p>
+
+                <button onClick={this.setFailRate.bind(this, 0)}>Fail none</button>
+                <button onClick={this.setFailRate.bind(this, 0.1)}>Fail 10%</button>
+                <button onClick={this.setFailRate.bind(this, 0.25)}>Fail 25%</button>
+                <button onClick={this.setFailRate.bind(this, 0.75)}>Fail 75%</button>
+                <button onClick={this.setFailRate.bind(this, 1)}>Fail all</button>
             </div>
         );
     }
